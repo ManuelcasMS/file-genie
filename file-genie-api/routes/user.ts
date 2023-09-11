25324d2359c5e3714f0cf9612bc93a65993c3836
ManuelcasMS <manuelcas@microsoft.com>
2023-09-11 15:59:11 -0700

    add openai endpoint

--- file-genie-api/routes/user.ts
+++ file-genie-api/routes/user.ts
@@ -3,9 +3,28 @@
  */
 import express = require('express');
 const router = express.Router();
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
-router.get('/', (req: express.Request, res: express.Response) => {
-    res.send("respond with a resource");
+
+async function test(question: string)
+{    
+    try{
+        const client = new OpenAIClient(
+        "https://openai-test-11.openai.azure.com/", 
+        new AzureKeyCredential("fc1426f188c74182bbdb6f591408d4db")
+        );
+        const response = await client.getChatCompletions("open-ai-test-11-chat",  [{"role":"user","content":question}]);
+        return response.choices[0].message.content;
+    }catch (err){
+        console.log(err);
+    }   
+    return false;
+}
+
+router.get('/', async (req: express.Request, res: express.Response) => {
+    const question = req.query.question.toString();
+    const resp =  await test(question);
+    res.send(resp);
 });
 
 export default router;
