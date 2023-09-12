7101ad7e2957f8ecf3d9b6443fec1ab2b917c98d
ManuelcasMS <manuelcas@microsoft.com>
2023-09-12 15:13:49 -0700

    improve open ai endpoint

--- file-genie-api/routes/user.ts
+++ file-genie-api/routes/user.ts
@@ -3,17 +3,25 @@
  */
 import express = require('express');
 const router = express.Router();
+var bodyParser = require('body-parser');
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
+// create application/json parser
+var jsonParser = bodyParser.json()
 
-async function test(question: string)
+interface ChatInfo {
+    content: string;
+    role: string;
+}
+
+async function test(chatInfo: ChatInfo[])
 {    
     try{
         const client = new OpenAIClient(
         "https://openai-test-11.openai.azure.com/", 
         new AzureKeyCredential("fc1426f188c74182bbdb6f591408d4db")
         );
-        const response = await client.getChatCompletions("open-ai-test-11-chat",  [{"role":"user","content":question}]);
+        const response = await client.getChatCompletions("open-ai-test-11-chat",  chatInfo);
         return response.choices[0].message.content;
     }catch (err){
         console.log(err);
@@ -21,9 +29,9 @@ async function test(question: string)
     return false;
 }
 
-router.get('/', async (req: express.Request, res: express.Response) => {
-    const question = req.query.question.toString();
-    const resp =  await test(question);
+router.post('/',jsonParser, async (req: express.Request, res: express.Response) => {
+    const chatInfo = req.body.chatInfo as ChatInfo[];
+    const resp =  await test(chatInfo);
     res.send(resp);
 });
 
