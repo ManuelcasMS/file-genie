2fd6e35b41c076808b394585196b58bea633cb97
Charan Teja Lellaboyena <clellaboyena@microsoft.com>
2023-09-13 13:16:22 -0700

    Upload file

--- file-genie-client/src/FileUpload.tsx
+++ file-genie-client/src/FileUpload.tsx
@@ -2,24 +2,46 @@ import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 
 import styles from "./styles/file.module.scss";
+import { API_HOST_URL } from "./util/constants";
 
 const fileTypes = ["JPEG", "CSV", "PDF"];
 
 export default function FileUpload() {
-  const [file, setFile] = useState<any>(null);
-  const handleChange = (file: any) => {
-    setFile(file);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [files, setFiles] = useState<File[]>([]);
+  const handleChange = (file: File[]) => {
+    setFiles(file);
+    console.log(file);
   };
+
+  
+   const uploadFile = async () =>  {
+    const formData  = new FormData();
+    const input_file = document.querySelector('input[type="file"]')
+
+    Array.from(input_file?.files).forEach((f: any) => {
+        formData.append('image[]', f)
+    })
+        
+    const response = await fetch(`${API_HOST_URL}/storage/container/file-genie/upload`, {
+      method: 'POST',
+      body: formData
+    });
+
+    console.log(response);
+  }
+
+
   return (
     <div className={styles.upload}>
       <h1>Drag & Drop Files</h1>
       <FileUploader
         multiple={true}
         handleChange={handleChange}
-        name="file"
+        name="uploaded_files"
         types={fileTypes}
       />
-      <p>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
+        <button onClick={uploadFile}>Upload</button>
     </div>
   );
 }
